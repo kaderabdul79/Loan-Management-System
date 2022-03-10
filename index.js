91f907f5e01762744b51a749ba2da34d3e8df2dc
kaderabdul <kaderabdul7980@gmail.com>
2022-03-10 21:22:08 +0600

    create get api named donates which fetch all info who donated

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
+const { MongoClient} = require('mongodb');
+
 const port = 5000
 
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://charitox:wdfmTpx8jpTkFja6@cluster0.vxcvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("charitox");
+    const donatesColllection = database.collection("donatesColllection");
+
+    // insert to donatesColllection
+    app.post('/donates', async (req, res) => {
+      const donate = req.body;
+      const result = await donatesColllection.insertOne(donate);
+      res.json(result)
+    });
+
+    // show to donatesColllection all data who donated us
+    app.get('/donates', async (req, res) => {
+      const query = {};
+      const cursor = donatesColllection.find(query);
+      const donates = await cursor.toArray();
+      res.json(donates);
+    });    
+
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// to test
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
