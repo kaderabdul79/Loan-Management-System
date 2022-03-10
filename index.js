6058864b98ae5f9edbcd3e7e1f97e2727fe0a929
kaderabdul <kaderabdul7980@gmail.com>
2022-03-10 22:35:54 +0600

    put userinfo to .env and .gitignore created

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient} = require('mongodb');
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://charitox:wdfmTpx8jpTkFja6@cluster0.vxcvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vxcvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
